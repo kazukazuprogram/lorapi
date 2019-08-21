a8ebbf32b45a6fb57f8652fb9587840b83c12c8d
kazukazuprogram <None>
2019-08-21 11:43:31 +0900

    Any

--- getgps.py
+++ getgps.py
@@ -30,14 +30,6 @@ gpsthread.start() # スレッドを起動
 while True:
     if gps.clean_sentences > 20: # ちゃんとしたデーターがある程度たまったら出力する
         h = gps.timestamp[0] if gps.timestamp[0] < 24 else gps.timestamp[0] - 24
-        # print('%2d:%02d:%04.1f' % (h, gps.timestamp[1], gps.timestamp[2]))
-        # print('緯度経度: %2.8f, %2.8f' % (gps.latitude[0], gps.longitude[0]))
-        # print('海抜: %f' % gps.altitude)
-        # print(gps.satellites_used)
-        # print('衛星番号: (仰角, 方位角, SN比)')
-        # for k, v in gps.satellite_data.items():
-        #     print('%d: %s' % (k, v))
-        # print('')
         data = {
             "lat" : "{:.7f}".format(gps.latitude[0]),
             "long" : "{:.7f}".format(gps.longitude[0])
